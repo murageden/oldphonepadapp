a65823c8364ec77a805328c96569ffbb844bcd10
Dennis Murage <muragemd@gmail.com>
2025-06-19 06:04:41 +0300

    initial commit (oldphonepadapp) : OldPhonePadApp

---
+++ OldPhonePadApp.Tests/OldPhonePadApp.Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OldPhonePadApp;
+
+namespace OldPhonePadApp.Tests
+{
+    [TestClass]
+    public class OldPhonePadTests
+    {
+        [TestMethod]
+        public void ConvertToOldPhonePad_EmptyString_ReturnsEmptyString()
+        {
+            string input = string.Empty;
+            string expected = string.Empty;
+
+            string result = OldPhonePadApp.OldPhonePad(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertToOldPhonePad_SimpleInput_ReturnsConvertedString()
+        {
+            string input = "4433555 555666#";
+            string expected = "HELLO";
+
+            string result = OldPhonePadApp.OldPhonePad(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ OldPhonePadApp/OldPhonePadApp.cs
@@ -0,0 +1,104 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace OldPhonePadApp;
+
+public class OldPhonePadApp
+
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter numbers (0-9) to simulate key press to old phone keypad:");
+        string input = Console.ReadLine();
+
+        string result = OldPhonePad(input);
+
+        Console.WriteLine("Old phone keypad result: " + result);
+    }
+
+    public static string OldPhonePad(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return string.Empty;
+        }
+
+        var phonePad = new Dictionary<char, string> {
+        {'1', ",.?!:;@$%&*()"},
+        {'2', "ABC"},
+        {'3', "DEF"},
+        {'4', "GHI"},
+        {'5', "JKL"},
+        {'6', "MNO"},
+        {'7', "PQRS"},
+        {'8', "TUV"},
+        {'9', "WXYZ"},
+        {'0', " "}
+    };
+
+        var result = new StringBuilder();
+        int i = 0;
+        int n = input.Length;
+
+        while (i < n)
+        {
+            char currentChar = input[i];
+
+            if (currentChar == '#')
+            {
+                break;
+            }
+            else if (currentChar == '*')
+            {
+                if (result.Length > 0)
+                {
+                    result.Length--;
+                }
+                i++;
+            }
+            else if (currentChar == ' ')
+            {
+                i++; // Space to next character
+            }
+            else
+            {
+                char key = currentChar;
+                int count = 0;
+                while (i < n && input[i] == key)
+                {
+                    count++;
+                    i++;
+                }
+                if (phonePad.ContainsKey(key))
+                {
+                    if (key == '0')
+                    {
+                        // Special case for '0' which maps to space
+                        result.Append(' ');
+                    }
+                    else
+                    {
+                        string alphabets = phonePad[key];
+                        int index = (count - 1) % alphabets.Length;
+                        result.Append(alphabets[index]);
+                    }
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+
+
+}  
+
+
+// The code above defines a program that converts a string to an old phone pad format.
+// It uses a dictionary to map characters to their corresponding phone pad numbers. 
+// The main method prompts the user for input and displays the converted result.
+// The OldPhonePad method handles the conversion logic.
