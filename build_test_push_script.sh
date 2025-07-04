b1e995d6e795e077d7c58a8afd9a07f58a937807
Dennis Murage <muragemd@gmail.com>
2025-07-04 23:37:47 +0300

    app (Fix Automated Testing): Auto Run Tests and Push Changes

--- build_test_push_script.sh
+++ build_test_push_script.sh
@@ -2,19 +2,19 @@
 
 TEST_COMMAND="dotnet test" 
 
-echo "Running Tests With: $TEST_COMMAND"
+echo "\n Automatically Running Tests and Pushing Changes to GitHub... \n"
 
 # Check for compile time errors
 cd OldPhonePadApp/
 dotnet build
 
 if [ $? -ne 0 ]; then
-    echo "Build failed. Could be errors in the code Exiting script."
+    echo "\n Build failed. Could be errors in the code Exiting script. \n"
     exit 1 # Failure
 fi
 
 # Run tests
-echo "Running Tests..."
+echo "\n Now Running Tests... \n"
 if [ ! -d "OldPhonePadApp.Tests" ]; then
     cd ../OldPhonePadApp.Tests
 fi
@@ -24,15 +24,15 @@ $TEST_COMMAND
 
 # Check if the test command was successful
 if [ $? -eq 0 ]; then
-    echo "All tests passed! Pushing to GitHub..."
+    echo "\n All tests passed! Pushing to GitHub... \n"
     # Add changes to git
     cd ../
     git add .
-    git commit -m "app (fix automated testing): Run tests and Push Changes"
+    git commit -m "app (Fix Automated Testing): Auto Run Tests and Push Changes"
     git push origin main
-    echo "Changes pushed successfully."
+    echo "\n Changes pushed successfully. \n"
     exit 0 # Success
 else
-    echo "Some Tests Failed. Push to Github will not be successful."
+    echo "\n Some Tests Failed. Push to Github will not be successful. \n"
     exit 1 # Failure
 fi
